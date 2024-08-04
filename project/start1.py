9f87aa0f97ff75acb9287c3bb0455e2d4a2dd0a2
AFTM25 <adityapratamay07@gmail.com>
2024-08-04 14:44:12 +0700

    Perubahan

--- project/start1.py
+++ project/start1.py
@@ -1,11 +1,3 @@
-print('Selamat Datang'.center(35,'='))
-
-angka1 = int(input('Masukkan bilangan : '))
-if angka1 % 2 == 0:
-    print('Bilangan Genap')
-else:
-    print('Bilangan Ganjil')
-
-if angka1 <= 10:
-    print('Angka pass')
+print('Selamat Datang')
+print('Ini adalah sebagai ide dasar pembelajaran python saja...')
         
