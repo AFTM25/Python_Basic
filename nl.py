52cf88cb6cb63d85ba63a2852233da2c21f81100
AFTM25 <adityapratamay07@gmail.com>
2024-08-04 14:17:09 +0700

    Cobaan Pertama

--- nl.py
+++
@@ -1,9 +0,0 @@
-# Tipe Data Sequence Biner
-
-b1 = bytes('Hello World', 'utf-8')
-print('Nilai dari b1 = ', b1)
-
-ba = bytearray(b1)
-mv = memoryview(ba)
-print('Lokasi memori dalam memoryview = ', mv)
-
