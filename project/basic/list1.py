53e0771f991706435728009c2889685fabf38f9e
AFTM25 <adityapratamay07@gmail.com>
2025-02-13 13:17:12 +0700

    Thu Feb 13 13:17:12 WIB 2025

--- project/basic/list1.py
+++ project/basic/list1.py
@@ -3,3 +3,8 @@ data_1    = data_buah.copy()
 data_1[2] = 'stroberi'
 print(f'Data buah = {data_buah}')
 print(f'Data copy buah = {data_1}')
+
+data_1.insert(2, 'nanas')
+for x, y in enumerate(data_1):
+    print(f'{x+1}.{y}')
+
